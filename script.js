c724571950e95ffc0d8095814943b4156d034dd6
VitorBernado <vitorhugobernardodeoliveira@gmail.com>
2023-07-12 12:23:41 -0300

    Projeto finalizado

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const inputEl = document.querySelector("#password")
 const upperCaseCheckEl = document.querySelector("#uppercase-check")
 const numberCheckEl = document.querySelector("#number-check")
 const symbolCheckEl = document.querySelector("#symbol-check")
-
+const scurityIndicatorBarEl = document.querySelector("#security-indicator-bar")
 let passwordLength = 16
 
 function generatePassword() {
@@ -32,6 +32,59 @@ function generatePassword() {
   }
 
   inputEl.value = password
+  calculeteQuality()
+  calculeteFontSize()
+}
+
+function calculeteQuality() {
+  const percent = Math.round((passwordLength / 64) * 25 +
+    (upperCaseCheckEl.checked ? 15 : 0) +
+    (numberCheckEl.checked ? 25 : 0) +
+    (symbolCheckEl.checked ? 35 : 0)
+  )
+  scurityIndicatorBarEl.style.width = `${percent}%`
+
+  if (percent == 100) {
+    scurityIndicatorBarEl.classList.remove('safe')
+    scurityIndicatorBarEl.classList.remove('warning')
+    scurityIndicatorBarEl.classList.remove('critical')
+    scurityIndicatorBarEl.classList.add('completed')
+  }else if (percent > 69) {
+    scurityIndicatorBarEl.classList.add('safe')
+    scurityIndicatorBarEl.classList.remove('warning')
+    scurityIndicatorBarEl.classList.remove('critical')
+    scurityIndicatorBarEl.classList.remove('completed')
+  }else if (percent > 45) {
+    scurityIndicatorBarEl.classList.remove('safe')
+    scurityIndicatorBarEl.classList.add('warning')
+    scurityIndicatorBarEl.classList.remove('critical')
+    scurityIndicatorBarEl.classList.remove('completed')
+  }else {
+    scurityIndicatorBarEl.classList.remove('safe')
+    scurityIndicatorBarEl.classList.remove('warning')
+    scurityIndicatorBarEl.classList.add('critical')
+    scurityIndicatorBarEl.classList.remove('completed')
+  }
+}
+
+function calculeteFontSize() {
+  if (passwordLength > 45) {
+    inputEl.classList.remove('font-sm')
+    inputEl.classList.remove('font-xs')
+    inputEl.classList.add('font-xxs')
+  }else if (passwordLength > 32) {
+    inputEl.classList.remove('font-sm')
+    inputEl.classList.remove('font-xxs')
+    inputEl.classList.add('font-xs')
+  }else if (passwordLength > 22) {
+    inputEl.classList.add('font-sm')
+    inputEl.classList.remove('font-xs')
+    inputEl.classList.remove('font-xxs')
+  }else {
+    inputEl.classList.remove('font-sm')
+    inputEl.classList.remove('font-xs')
+    inputEl.classList.remove('font-xxs')
+  }
 }
 
 function copy() {
@@ -51,5 +104,6 @@ symbolCheckEl.addEventListener('click', generatePassword)
 
 document.querySelector("#copy-1").addEventListener('click', copy)
 document.querySelector("#copy-2").addEventListener('click', copy)
+document.querySelector("#renew").addEventListener('click', generatePassword)
 
 generatePassword()
